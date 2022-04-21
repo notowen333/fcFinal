817cb23b85d7a7d20487dac2d68d04a53d2f9d26
Owen Kaplan <owenkaplan@Owens-MacBook-Pro.local>
2022-04-21 11:33:02 -0400

    working example

---
+++ sketch.js
@@ -0,0 +1,18 @@
+/**
+ * Owen Kaplan
+ * 
+ * Form and Code Final Project
+ * 
+ */
+
+
+function setup() {
+
+    createCanvas(800, 800);
+
+}
+
+function draw() {
+
+    ellipse(40, 40, 80, 80);
+}
